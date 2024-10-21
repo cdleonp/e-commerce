86e8369637418450bd4306c1024a2110fc2005aa
cdleonp <christdavlnpz@gmail.com>
2024-10-21 17:25:59 -0300

    feature/Adding header and cart functionality with a service

---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,49 @@
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  // @Input({ required: true }) cartItems: Product[] = [];
+  private cartService = inject(CartService);
+
+  hideCartMenu = signal(true);
+  cart = this.cartService.cart;
+  totalPrice = this.cartService.totalPrice;
+
+  toggleCartMenu() {
+    this.hideCartMenu.update(prevState => !prevState);
+  }
+
+  removeProductFromCart(id: number) {
+    // console.log('Estoy en el padre');
+    this.cartService.removeProductFromCart(id);
+  }
+
+  /* Sin el servicio */
+
+  // calcTotalPrice() {
+  //   return this.cartItems.reduce((total, product) => total + product.price, 0);
+  // }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('ngOnChanges');
+  //   console.log('-'.repeat(10));
+  //   console.log(changes);
+  //   if(changes.hasOwnProperty('cartItems')) {
+  //     this.totalPrice.set(this.calcTotalPrice());
+  //   }
+  // }
+
+  // removeProductFromCart(id: number) {
+  //   const carItemsFiltered = this.cartItems.filter(product => product.id !== id);
+  //   this.cartItems = carItemsFiltered;
+  //   console.log(this.cartItems);
+  // }
+}
